9bfdefd7842afba3e1c990f42d5212a21240570c
Saurav Chamoli <sauravchamoli17@gmail.com>
2022-06-19 21:50:32 +0530

    Commit

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,2 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.10;
 
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+      console.log('This is my DOMAIN CONTRACT!');
+    }
+}   
